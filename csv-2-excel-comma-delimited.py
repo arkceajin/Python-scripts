0fbbd0be086cfb62e44cf255561e64f0ff9a5d83
arkceajin <arkceajin@gmail.com>
2018-04-04 09:35:00 +0900

    Update csv-2-excel-comma-delimited.py

--- csv-2-excel-comma-delimited.py
+++ csv-2-excel-comma-delimited.py
@@ -7,7 +7,7 @@ for csvfile in glob.glob(os.path.join('.', '*.csv')):
     wb = openpyxl.Workbook()
     ws = wb.active
     with open(csvfile, 'rb') as f:
-        reader = csv.reader(f, delimiter='\t')
+        reader = csv.reader(f)
         for r, row in enumerate(reader, start=1):
             for c, val in enumerate(row, start=1):
                 ws.cell(row=r, column=c).value = val
